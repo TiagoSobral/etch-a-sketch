6236d759726bc3e011efdcafc959bde8ec8ea96b
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-01-28 23:08:59 +0000

    (feat) add numbered class names to column divs.

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,8 @@
 const grid = document.querySelector(".grid");
 
-// function createGrid() {
-//     const column = document.createElement("div");
-//     grid.appendChild(column);
-// };
-
 for (let i = 1; i <= 16; i++) {
     let div = document.createElement("div");
+    div.classList.toggle(`column-${i}`);
     grid.appendChild(div);
 }
+
