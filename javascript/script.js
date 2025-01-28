5773641b7d9fcb209c74452c2446dc0cdbe9ec89
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-01-28 23:16:18 +0000

    (feat) nested loop to add 16 blocks insd columnDiv

--- javascript/script.js
+++ javascript/script.js
@@ -4,5 +4,9 @@ for (let i = 1; i <= 16; i++) {
     let columnDiv = document.createElement("div");
     columnDiv.classList.toggle(`column-${i}`);
     grid.appendChild(columnDiv);
+    for (let j = 1; j <= 16; j++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.toggle(`row-${j}`);
+        columnDiv.appendChild(rowDiv);
     }
-
+}
