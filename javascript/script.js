ab57ed722932a5517cb3a22584d10d7dd4dd9979
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-01-28 23:15:47 +0000

    (fix) change name from div to columnDiv.

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,8 @@
 const grid = document.querySelector(".grid");
 
 for (let i = 1; i <= 16; i++) {
-    let div = document.createElement("div");
-    div.classList.toggle(`column-${i}`);
-    grid.appendChild(div);
-}
+    let columnDiv = document.createElement("div");
+    columnDiv.classList.toggle(`column-${i}`);
+    grid.appendChild(columnDiv);
+    }
 
