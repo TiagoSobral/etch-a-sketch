28d6d4a73aaa49167381f13d228a13b9bae1f444
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-01-28 22:41:06 +0000

    (feat) create loop to create 16 divs.

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+const grid = document.querySelector(".grid");
+
+function createGrid {
+    const column = document.createElement("div");
+    grid.appendChild(column);
+}
+
+for (let i = 1; i <= 16, i++) {
+    createGrid();
+}
