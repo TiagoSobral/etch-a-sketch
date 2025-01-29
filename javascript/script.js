ccdc0b0111e5b3df25a4949140bb192897be0a18
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-01-29 23:08:48 +0000

    (fix) change from forEach to for..of iterator.

--- javascript/script.js
+++ javascript/script.js
@@ -14,9 +14,8 @@ for (let i = 1; i <= 16; i++) {
 const columns = document.querySelectorAll(".column");
 const rows = document.querySelectorAll(".row");
 
-rows.forEach( (row) => {
+for (const row of rows) {
     row.addEventListener("mouseenter", () => {
         row.style.backgroundColor = "red";
     });
-});
-
+};
