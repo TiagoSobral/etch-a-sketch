587458115df16f6d82aea0de627ac709ab02cdcb
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-01-29 22:53:07 +0000

    (feat) change color of square when hover.
    querySelectorAll needs to be placed after the loop.
    
    forEach loop in Row to have all the squares with
    eventListener

--- javascript/script.js
+++ javascript/script.js
@@ -2,11 +2,21 @@ const grid = document.querySelector(".grid");
 
 for (let i = 1; i <= 16; i++) {
     let columnDiv = document.createElement("div");
-    columnDiv.classList.toggle(`column-${i}`);
+    columnDiv.classList.toggle("column");
     grid.appendChild(columnDiv);
     for (let j = 1; j <= 16; j++) {
         let rowDiv = document.createElement("div");
-        rowDiv.classList.toggle(`row-${j}`);
+        rowDiv.classList.toggle("row");
         columnDiv.appendChild(rowDiv);
     }
 }
+
+const columns = document.querySelectorAll(".column");
+const rows = document.querySelectorAll(".row");
+
+rows.forEach( (row) => {
+    row.addEventListener("mouseenter", () => {
+        row.style.backgroundColor = "red";
+    });
+});
+
